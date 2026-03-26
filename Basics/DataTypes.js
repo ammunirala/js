02fa3da316050ec0e47fa0829c5b9ab6b08fd380
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2026-03-26 06:11:23 +0000

    Datatypes of javascript

---
+++ Basics/DataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+// alert( 3 + 3) // we are using nodejs, not browser
+console.log(3+3) 
+console.log("ammu")
+
+
+let name = "ammu" //string
+let age = 18 // number
+let isLoggedIn = false // boolean
+
+// data types 
+// number => 2 to power 53
+//bigint
+//string => ""
+//bolean => true/false 
+//null => standalone value , empty value
+//undefined => not assigned
+//symbol => unique
+//object 
+
+
+console.log(typeof "ammu"); // string
+console.log(typeof null); //object
+console.log(typeof undefined);  // undefined
