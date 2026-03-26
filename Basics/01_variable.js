5b4117ddba272a4b846deb94d6a6a4dd31e8cdae
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2026-03-26 05:57:51 +0000

    variables

---
+++ Basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = "144553"
+let accountEmail = "aariyanirala1@gmail.com"
+var accountPassword = "ammunirala@123"
+accountCity = "Dehradun"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "hdgha@hh.com"
+accountPassword  = "54666696"
+accountCity = "Delhi"
+// console.log = (accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
