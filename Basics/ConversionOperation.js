b9b6ad3ce67cacad9e64d5f4600ebd2d80f25e0b
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2026-03-27 13:27:24 +0000

    comparision

--- Basics/ConversionOperation.js
+++ Basics/ConversionOperation.js
@@ -1,27 +1,41 @@
-let score = "33abs"
+// let score = "33abs"
 
-console.log(typeof score);
-console.log(typeof(score));
-
-
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
-//"33" => 33
-//"33abs" => Nan
-//true => 1; false => 0
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-//1=> true; 0=> false 
-//""=>false
-//"ammu"=> true
 
-let someNumber = 33
-let StringNumber = String(someNumber)
-console.log(stringNumber);
+// //"33" => 33
+// //"33abs" => Nan
+// //true => 1; false => 0
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-console.log(typeof stringNumber);
+// //1=> true; 0=> false 
+// //""=>false
+// //"ammu"=> true
+
+// let someNumber = 33
+// let StringNumber = String(someNumber)
+// console.log(stringNumber);
+
+// console.log(typeof stringNumber);
+
+// ********** operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
