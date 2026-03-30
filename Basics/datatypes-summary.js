f871247b4d3c93ab07737701670f7e7f67ad88be
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2026-03-30 06:33:33 +0000

    Stack and Heap

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -23,11 +23,37 @@ const bigNumber = 6153636343n
 
 //Reference (Non Primitive) data types : (Array, Objects, Functions)
 
-const heros = ["virat kohli", "modi ji", "dogga", "naagraj"]
-let myObj = {  
-  name : "ammu",
-  age : 25,
+// const heros = ["virat kohli", "modi ji", "dogga", "naagraj"]
+// let myObj = {  
+//   name : "ammu",
+//   age : 25,
 
 
+// }
+
+// +++++++++++++++++++++++++++++++++++
+
+
+// stack (Primitive) , Heap (Non- Primitive)
+
+let myYoutubename = "Amresh.com" // stored in stack
+
+let anotherName = "Nirala.com" // stored in stack
+
+console.log(myYoutubename); // Amresh.com
+
+console.log(anotherName);
+
+
+let user1 = {
+  email: "user@google.com",
+  upi: "user@ybl"
 }
+let user2 = user1
+user2.email = "amresh@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+
 
