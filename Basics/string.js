c0f8d0a92db71fd2baa1cd3900b9ae0fb812291d
Amresh Nirala <138843165+ammunirala@users.noreply.github.com>
2026-03-30 07:36:21 +0000

    string

---
+++ Basics/string.js
@@ -0,0 +1,23 @@
+const name = "amresh"
+const repoCount = 20
+
+// console.log(name + repoCount + "Value");
+
+
+console.log(`hello my name is ${name} and my repo count is $ {repoCount}`);
+
+const gameName = "AmmuBhai"
+console.log(gameName[0]);
+console.log(gameName.length);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+console.log(gameName.toUpperCase());
+
+
+
+
+
